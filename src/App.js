be51329cf57e9eb717d7e6a311c595a0e3ca490c
Enu Azubike <enuazubike88@gmail.com>
2022-05-03 16:42:10 +0100

    added header component and links to home page and shop page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Outlet />
     </div>
   );
