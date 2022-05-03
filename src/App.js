352959b90f7f4265af128fa3639946743e3c0e80
Enu Azubike <enuazubike88@gmail.com>
2022-05-03 15:14:44 +0100

    updated our menu-items to allow navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/pages/homepage/homepage.component';
+
+const Hats = (props) => {
+  return (
+    <div>
+      <h1>{`Hats Page`.toUpperCase()}</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/hats" element={<Hats />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
